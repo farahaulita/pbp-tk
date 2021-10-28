4a5707c9f4d2fef026e70910a36d813ee76d8dea
AditSukoco <raditskc@gmail.com>
2021-10-28 12:18:01 +0700

    landing page modified

--- main/models.py
+++ main/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Suggestion(models.Model):
+    name = models.CharField(max_length=30)
+    email = models.EmailField()
+    message = models.TextField(max_length=200)
