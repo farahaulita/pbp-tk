cf6917ec963bafcd80228a25134e4f4542a572e5
AditSukoco <raditskc@gmail.com>
2021-11-04 13:26:43 +0700

    ajax on feedback form

--- main/static/js/success_page_script.js
+++ main/static/js/success_page_script.js
@@ -1,1 +1,16 @@
-setTimeout(function(){ window.navigate('../') }, 3000)
+$(document).ready( function() {
+    $.ajax(
+        {
+            url: "/suggestion_json",
+            success: function(result) {
+                $("#name").text(result.name)
+                $("#email").text(result.email)
+                $("#message").text(result.message)
+            },
+        }
+    )
+})
+
+setTimeout(function(){ 
+    location.href = "../"
+}, 10000)
--- main/views.py
+++ main/views.py
@@ -1,19 +1,27 @@
 from django.shortcuts import render, redirect
 from django.http import JsonResponse
-import json
 from .forms import SuggestionForm
 
-latest_response = None
+latest_response = False
+name = None
+email = None
+message = None
 
 def home(request):
     global latest_response
+    global name
+    global email
+    global message
     context = {}
 
     form = SuggestionForm(request.POST or None, request.FILES or None)
     if request.method == 'POST':
         form = SuggestionForm(request.POST)
         if form.is_valid():
-            latest_response = convert_suggestion_to_json(request.POST.get('name'), request.POST.get('email'), request.POST.get('message'))
+            latest_response = True
+            name = request.POST.get('name')
+            email = request.POST.get('email')
+            message = request.POST.get('message')
             form.save()
             if request.method == 'POST':
                 return redirect('main:success')
@@ -25,20 +33,16 @@ def home(request):
 def success_page(request):
     global latest_response
 
-    if latest_response == None:
+    if not latest_response:
         return redirect('main:home')
-
-    data = json.loads(latest_response.content)
-    # Get data from JSON and reset JSON
-    response = {
-        'name' : data['name'],
-        'email' : data['email'],
-        'message' : data['message'],
-    }
-    latest_response = None
     
-    return render(request, 'main/success.html', response)
+    latest_response = False
+    
+    return render(request, 'main/success.html')
 
 
-def convert_suggestion_to_json(name, email, message):
+def save_json(request):
+    global name
+    global email
+    global message
     return JsonResponse({'name': name, 'email' : email, 'message' : message})
