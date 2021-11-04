aa1c82b8bb220c875ed3159cda86396b025970b6
AditSukoco <raditskc@gmail.com>
2021-11-04 14:32:10 +0700

    renewed login

--- login/urls.py
+++ login/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from . import views
 
+app_name = 'login'
+
 urlpatterns = [
 #     path('', views.index, name= 'index'),
-    path('', views.login_view, name='login'),
+    path('', views.login_view, name='home'),
     path('student/', views.student, name='student'),
     path('teacher/', views.teacher, name='teacher'),
 
--- project_name/settings.py
+++ project_name/settings.py
@@ -169,5 +169,5 @@ for directory in [*STATICFILES_DIRS, STATIC_ROOT]:
 
 # Enable compression and caching features of whitenoise.
 # You can remove this if it causes problems on your setup.
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE =  'django.contrib.staticfiles.storage.StaticFilesStorage' 
 
