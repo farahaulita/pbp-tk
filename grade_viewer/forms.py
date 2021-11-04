0f35161e6f05ca9811bb8ec6c1f72f697cecccaf
Bimasena Putra <bimasenaputra@gmail.com>
2021-11-04 14:51:04 +0700

    Update form & template

--- grade_viewer/forms.py
+++ grade_viewer/forms.py
@@ -1,4 +1,4 @@
 from django import forms
 
 class QuestionForm(forms.Form):
-    question = forms.CharField(widget=forms.Textarea(attrs={'placeholder': 'Enter your suggestions or critics'}),label="")
+    question = forms.CharField(widget=forms.Textarea(attrs={'placeholder': 'Enter your suggestions or critics','rows':10,'cols':30}),label="")
