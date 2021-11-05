57fab3f77fb91d6696936b361beac858fe7bd422
AditSukoco <raditskc@gmail.com>
2021-11-06 01:10:27 +0700

    how is this not fixed yet bruh

--- project_name/settings.py
+++ project_name/settings.py
@@ -182,3 +182,19 @@ MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 
 MEDIA_URL = '/media/'
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+             'level': os.getenv('DJANGO_LOG_LEVEL', 'DEBUG'),
+        },
+    },
+}
+
